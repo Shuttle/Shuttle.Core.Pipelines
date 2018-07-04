1b5f4678eb5358b52dcf1863045c9db521c5a65b
eben-roux <me@ebenroux.co.za>
2018-07-04 17:44:34 +0200

    - v10.0.6

--- Shuttle.Core.Pipelines/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Pipelines/Properties/AssemblyInfo.cs
@@ -25,14 +25,18 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Core 2.0")]
 #endif
 
+#if NETCOREAPP2_1
+[assembly: AssemblyTitle(".NET Core 2.1")]
+#endif
+
 #if NETSTANDARD2_0
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.5.0")]
+[assembly: AssemblyVersion("10.0.6.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
 [assembly: AssemblyProduct("Shuttle.Core.Pipelines")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.5")]
+[assembly: AssemblyInformationalVersion("10.0.6")]
 [assembly: ComVisible(false)]
