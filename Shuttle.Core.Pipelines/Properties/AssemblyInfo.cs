e4608dc076c42fd35b6dcf4d3366cd12743d0ae4
Eben <me@ebenroux.co.za>
2022-04-09 15:37:57 +0200

    - package upgrade

--- Shuttle.Core.Pipelines/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Pipelines/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard")]
 #endif
 
-[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyVersion("11.0.1.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2022, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Core.Pipelines")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("11.0.0")]
+[assembly: AssemblyInformationalVersion("11.0.1")]
 [assembly: ComVisible(false)]
